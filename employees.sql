183b3ba3f75697ce17decf7852754743ddcf8bf8
Mary Cohan <mary.cohan@gmail.com>
2021-03-16 13:04:18 -0700

    setting up employees.sql

---
+++ employees.sql
@@ -0,0 +1,34 @@
+DROP DATABASE IF EXISTS employee_db
+CREATE DATABASE employee_db
+USE employee_db
+
+CREATE TABLE departments(
+    id INT AUTO_INCEREMENT NOT NULL,
+    name VARCHAR(30),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE roles(
+    id INT AUTO_INCEREMENT NOT NULL,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE employees(
+    id INT AUTO_INCEREMENT NOT NULL,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT
+    manager_id INT
+    PRIMARY KEY (id)
+);
+
+SELECT name, title, salary
+FROM roles
+INNER JOIN departments ON roles.department_id = departments.id
+
+SELECT name, title, salary
+FROM roles
+INNER JOIN departments ON roles.department_id = departments.id
