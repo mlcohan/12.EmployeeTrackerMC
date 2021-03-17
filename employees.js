cae8b9b15eacb45c9c6b1315f4bc1562f1c70420
Mary Cohan <mary.cohan@gmail.com>
2021-03-17 12:42:29 -0700

    add packages as top of emplloyees,js

--- employees.js
+++ employees.js
@@ -1,4 +1,7 @@
 const mysql = require('mysql');
+const inquirer = require('inquirer');
+const consoleTable = require('console.table');
+
 
 const connection = mysql.createConnection({
     host: 'localhost',
